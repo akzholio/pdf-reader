4c9ecc926472031d8ed2ea5104f3cc966e8e1cda
Akzhol Imangaliyev <akzhol.imangaliyev@yougov.com>
2024-04-26 17:33:45 +0200

    Poppler first steps

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st  # type: ignore
-import poppler as pl
+from poppler import load_from_data
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
-    document = pl.load_from_data(bytes_data)
-    for page in document.pages:
-        st.write(page)
+    pdf_document = load_from_data(bytes_data)
+    page_1 = pdf_document.create_page(0)
+    page_1_text = page_1.text()
+    st.write(page_1_text)
